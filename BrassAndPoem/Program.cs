dbe18b189de6ba80d825b3f64fb22d510724d36c
Sean Bryant <smbryant1730@gmail.com>
2023-07-18 21:46:09 -0400

    setup

--- BrassAndPoem/Program.cs
+++ BrassAndPoem/Program.cs
@@ -1,10 +1,56 @@
 ﻿
 //create a "products" variable here to include at least five Product instances. Give them appropriate ProductTypeIds.
-
+List<Product> products = new List<Product>()
+{
+    new Product()
+    {
+        Name = "Flugelhorn",
+        Price = 199.99M,
+        ProductTypeId = 1
+    },
+    new Product()
+    {
+        Name = "The Road Not Taken",
+        Price = 10.28M,
+        ProductTypeId = 2
+    },
+    new Product()
+    {
+        Name = "Trumpet",
+        Price = 849.99M,
+        ProductTypeId = 1
+    },
+    new Product()
+    {
+        Name = "Trombone",
+        Price = 349.99M,
+        ProductTypeId = 1
+    },
+    new Product()
+    {
+        Name = "O Captain! My Captain!",
+        Price = 12.26M,
+        ProductTypeId = 2
+    },
+};
 //create a "productTypes" variable here with a List of ProductTypes, and add "Brass" and "Poem" types to the List. 
-
+List<ProductType> productTypes = new List<ProductType>()
+{
+    new ProductType()
+    {
+        Title = "Brass",
+        Id = 1
+    },
+    new ProductType()
+    {
+        Title = "Poem",
+        Id = 2
+    }
+};
 //put your greeting here
-
+string greeting = @"Welcome to Brass and Poems!
+Your one-stop shop for Brass Instruments and classic Poems!";
+Console.WriteLine(greeting);
 //implement your loop here
 
 void DisplayMenu()
